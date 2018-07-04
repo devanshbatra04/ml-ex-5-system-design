7b86ab4f1f429b48247a0ab2c1dbb41ea41030b7
Devansh Batra <devanshbatra04@gmail.com>
2018-07-05 04:32:49 +0530

    assignment complete

--- ex5/learningCurve.m
+++ ex5/learningCurve.m
@@ -56,6 +56,7 @@ error_val   = zeros(m, 1);
 for i=1:m
   Xtrain =  X(1:i, :);
 	Ytrain = y(1:i);
+  fprintf("Lambda is %f", lambda)
 	theta = trainLinearReg(Xtrain,Ytrain,lambda);
 	error_train(i) = linearRegCostFunction(Xtrain,Ytrain,theta,0);
 	error_val(i) = linearRegCostFunction(Xval,yval,theta,0);
